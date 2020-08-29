83af3b6fda70453fc4e225dc462947764bda356f
natpoveda <ndpoveda@gmail.com>
2020-08-28 21:48:19 -0500

    Updates

---
+++ precache-manifest.d4422a9aea972dbd5badecb853e5b127.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98e5f2c3eca01825b62de385fcb53ac2",
+    "url": "/store-natalia-poveda/index.html"
+  },
+  {
+    "revision": "4ad2b17cb406c18d735b",
+    "url": "/store-natalia-poveda/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "29996ac399f0ee50f3ab",
+    "url": "/store-natalia-poveda/static/js/2.6b441bad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/store-natalia-poveda/static/js/2.6b441bad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ad2b17cb406c18d735b",
+    "url": "/store-natalia-poveda/static/js/main.fca2ee35.chunk.js"
+  },
+  {
+    "revision": "1d95d1da88a3a049cd29",
+    "url": "/store-natalia-poveda/static/js/runtime-main.3e3d5f10.js"
+  }
+]);
